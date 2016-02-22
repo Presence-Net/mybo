c1b8b12a7fb87fa06e7fd0faf9c29f54b99a6111
Christophe Faribault <christophefaribault@gmail.com>
2016-02-22 00:49:13 -0500

    Added country, locale, currency to user, updated reg form, localized dollar amounts in calendar

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -32,7 +32,7 @@ class User extends BaseUser
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
      *
-     * @Assert\NotBlank(message="Please enter your name.", groups={"Registration", "Profile"})
+     * @Assert\NotBlank(message="Please enter your first name.", groups={"Registration", "Profile"})
      * @Assert\Length(
      *     min=3,
      *     max="255",
@@ -46,7 +46,7 @@ class User extends BaseUser
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
      *
-     * @Assert\NotBlank(message="Please enter your name.", groups={"Registration", "Profile"})
+     * @Assert\NotBlank(message="Please enter your last name.", groups={"Registration", "Profile"})
      * @Assert\Length(
      *     min=3,
      *     max="255",
@@ -56,6 +56,27 @@ class User extends BaseUser
      * )
      */
     private $last_name;
+    
+    /**
+     * @ORM\Column(type="string", length=2, nullable=true)
+     *
+     * @Assert\NotBlank(message="Please select your country.", groups={"Registration", "Profile"})
+     */
+    private $country;
+    
+    /**
+     * @ORM\Column(type="string", length=5, nullable=true)
+     *
+     * @Assert\NotBlank(message="Please select your locale.", groups={"Registration", "Profile"})
+     */
+    private $locale;
+    
+    /**
+     * @ORM\Column(type="string", length=3, nullable=true)
+     *
+     * @Assert\NotBlank(message="Please select your currency.", groups={"Registration", "Profile"})
+     */
+    private $currency;
     
     /**
      * @ORM\OneToMany(targetEntity="Operation", mappedBy="user")
@@ -124,6 +145,78 @@ class User extends BaseUser
     public function setLastName($last_name)
     {
         $this->last_name = $last_name;
+
+        return $this;
+    }
+
+    /**
+     * Gets the country.
+     *
+     * @return mixed
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+    /**
+     * Sets the country.
+     *
+     * @param mixed $country the country
+     *
+     * @return self
+     */
+    public function setCountry($country)
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    /**
+     * Gets the locale.
+     *
+     * @return mixed
+     */
+    public function getLocale()
+    {
+        return $this->locale;
+    }
+
+    /**
+     * Sets the locale.
+     *
+     * @param mixed $locale the locale
+     *
+     * @return self
+     */
+    public function setLocale($locale)
+    {
+        $this->locale = $locale;
+
+        return $this;
+    }
+
+    /**
+     * Gets the currency.
+     *
+     * @return mixed
+     */
+    public function getCurrency()
+    {
+        return $this->currency;
+    }
+
+    /**
+     * Sets the currency.
+     *
+     * @param mixed $currency the currency
+     *
+     * @return self
+     */
+    public function setCurrency($currency)
+    {
+        $this->currency = $currency;
 
         return $this;
     }
--- src/AppBundle/Form/Type/RegistrationFormType.php
+++ src/AppBundle/Form/Type/RegistrationFormType.php
@@ -5,13 +5,37 @@ namespace AppBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
+use Symfony\Component\Form\Extension\Core\Type\LocaleType;
+use Symfony\Component\Form\Extension\Core\Type\CurrencyType;
+
+use Symfony\Component\Intl\Intl;
+
 class RegistrationFormType extends AbstractType
 {
+    private $container;
+    private $session;
+    
+    public function __construct($class, $container, $session)
+    {
+        $this->container = $container;
+        $this->session = $session;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         // add your custom field
         $builder->add('first_name');
         $builder->add('last_name');
+        $builder->add('country', CountryType::class, [
+            'data' => 'CA',
+        ]);
+        $builder->add('locale', LocaleType::class, [
+            'data' => 'en_CA',
+        ]);
+        $builder->add('currency', CurrencyType::class, [
+            'data' => 'CAD',
+        ]);
 
         $builder->remove('username');
     }
